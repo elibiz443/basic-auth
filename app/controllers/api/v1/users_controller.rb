21da92dd81d400b991faa6a1306cdc1fc5dd559d
elibiz443 <ellyambet10@gmail.com>
2023-05-04 19:57:31 +0300

    added create action in user API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,16 @@
 class Api::V1::UsersController < ApplicationController
-  def index
+  def create
+    @user = User.new(user_params)
+    if @user
+      render json: { message: "User created successfully 👍", user: @user }, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:full_name, :email, :role, :password_digest)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :payments
   has_many :notifications
   has_many :movies
+
+  validates :full_name, presence: true
+  validates :role, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: {with: URI::MailTo::EMAIL_REGEXP}
 end
