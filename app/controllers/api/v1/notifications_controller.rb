c9bd1f353c0b8ae97e14a78c073547c29e6568a5
elibiz443 <ellyambet10@gmail.com>
2023-05-05 23:52:58 +0300

    Added mailer

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -3,6 +3,10 @@ class Api::V1::NotificationsController < ApplicationController
     @notification = Notification.new(notification_params)
 
     if @notification.save
+      info = @notification.detail
+      recipient_email = User.find_by_id(@notification.user_id).email
+      MailServices.new.send_mail(info, recipient_email)
+
       render json: { message: "Notification sent 👍", notification: @notification }, status: :created
     else
       render json: { errors: @notification.errors.full_messages }, status: :unprocessable_entity
@@ -12,6 +16,6 @@ class Api::V1::NotificationsController < ApplicationController
   private
 
   def notification_params
-    params.permit(:detail, :status, :role, :user_id)
+    params.require(:notification).permit(:detail, :status, :role, :user_id)
   end
 end
