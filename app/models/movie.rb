b755ffbe1d1d8cf215ae2f575b8b30f9bade4fad
tuta2 <reyreghan20@gmail.com>
2023-05-08 22:43:01 +0300

    added movie validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ApplicationRecord
   belongs_to :user
+
+  validates :movie_link, presence: true, uniqueness: true
+  validates :movie_title, presence: true
+  validates :movie_type, presence: true
+  validates :release_date, presence: true 
 end
